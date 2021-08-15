a833fcb61f570d84c51717a935761d755c4997fd
Pau <frikeldon@gmail.com>
2021-08-15 17:59:02 +0200

    La propiedad name del mixin Field no es requerida

--- mixin/field.js
+++ mixin/field.js
@@ -14,7 +14,7 @@ function ancestorOfType (type, vnode) {
 export default {
   props: {
     /** Nombre del campo en el formulario. */
-    name: { type: String, required: true },
+    name: { type: String, default: null },
     /** Valor del campo. */
     modelValue: { type: null, default: undefined }
   },
@@ -33,9 +33,11 @@ export default {
     loadData () {}
   },
   mounted () {
-    this.form = ancestorOfType(CollectionField, this.$parent)
-    if (this.form) {
-      this.form.registerField(this)
+    if (this.name !== null) {
+      this.form = ancestorOfType(CollectionField, this.$parent)
+      if (this.form) {
+        this.form.registerField(this)
+      }
     }
   },
   beforeUnmount () {

a06bda80c1220cb1b3ee7c0b8e1bb610a4784230
Pau <frikeldon@gmail.com>
2021-08-19 21:32:52 +0200

    Correccion de la directiva content

--- utils/directive-content.js
+++ utils/directive-content.js
@@ -1,13 +1,11 @@
-export default {
-  mounted (el, binding) {
-    if (binding.arg.htmlify) {
-      el.innerHTML = binding.arg.htmlify(binding.value)
-    } else if (binding.arg.stringify) {
-      el.textContent = binding.arg.stringify(binding.value)
-    } else if (Array.isArray(binding.value)) {
-      el.textContent = binding.value.join(', ')
-    } else {
-      el.textContent = binding.value
-    }
+export default (el, binding) => {
+  if (binding.arg.htmlify) {
+    el.innerHTML = binding.arg.htmlify(binding.value)
+  } else if (binding.arg.stringify) {
+    el.textContent = binding.arg.stringify(binding.value)
+  } else if (Array.isArray(binding.value)) {
+    el.textContent = binding.value.join(', ')
+  } else {
+    el.textContent = binding.value
   }
 }

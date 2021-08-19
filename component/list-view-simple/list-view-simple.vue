7e5208e273e775b4d8bc4ebe0b3725f6c8172419
Pau <frikeldon@gmail.com>
2021-08-19 21:17:48 +0200

    Se mueven metodos al modulo utils/properties

--- component/list-view-simple/list-view-simple.vue
+++ component/list-view-simple/list-view-simple.vue
@@ -1,8 +1,9 @@
 <script>
 import FuraDetailsList from 'fura-vue/component/details-list/index.js'
 import FuraSpinNav from 'fura-vue/component/spin-nav/index.js'
-import { requestGet } from '../../utils/odata.js'
 import directiveContent from '../../utils/directive-content.js'
+import { requestGet } from '../../utils/odata.js'
+import { getValueFromPath, parsePropertiesFromPath } from '../../utils/properties.js'
 
 function getInitialState () {
   return {
@@ -10,87 +11,6 @@ function getInitialState () {
     selectedIndices: [],
     currentPage: 0,
     entitesLoaded: 0
-  }
-}
-
-function isPathMultiple (path) {
-  return path.split('/').some(step => step[0] === '*')
-}
-
-function getValueFromPath (path, entity) {
-  if (typeof path === 'string') {
-    if (isPathMultiple(path)) {
-      const steps = path.split('/')
-      const stepMultiple = steps.findIndex(step => step[0] === '*')
-      const stepsToArray = steps.splice(0, stepMultiple + 1)
-      stepsToArray[stepMultiple] = stepsToArray[stepMultiple].substr(1)
-
-      let level = entity
-      while (level && stepsToArray.length > 0) {
-        level = level[stepsToArray.shift()]
-      }
-
-      if (steps.length === 0) {
-        return level
-      } else {
-        return level.map(root => {
-          const localSteps = [...steps]
-          let current = root
-          while (current && localSteps.length > 0) {
-            current = current[localSteps.shift()]
-          }
-          return current
-        })
-      }
-    } else {
-      const steps = path.split('/')
-      let level = entity
-      while (level && steps.length > 0) {
-        level = level[steps.shift()]
-      }
-      return level
-    }
-  }
-  return undefined
-}
-
-function parsePropertiesFromPath (property, entity) {
-  if (isPathMultiple(property.path)) {
-    const steps = property.path.split('/')
-    const stepMultiple = steps.findIndex(step => step[0] === '*')
-    const stepsToArray = steps.splice(0, stepMultiple + 1)
-    stepsToArray[stepMultiple] = stepsToArray[stepMultiple].substr(1)
-
-    if (steps.length === 0) {
-      let level = entity
-      while (level && stepsToArray.length > 1) {
-        level = level[stepsToArray.shift()]
-      }
-      const last = steps[0]
-      level[last] = level[last].map(item => property.parse(item, property))
-    } else {
-      let level = entity
-      while (level && stepsToArray.length > 0) {
-        level = level[stepsToArray.shift()]
-      }
-      level.forEach(root => {
-        const localSteps = [...steps]
-        let current = root
-        while (current && localSteps.length > 1) {
-          current = current[localSteps.shift()]
-        }
-        const last = localSteps[0]
-        current[last] = property.parse(current[last], property)
-      })
-    }
-  } else {
-    const steps = property.path.split('/')
-    let level = entity
-    while (level && steps.length > 1) {
-      level = level[steps.shift()]
-    }
-    const last = steps[0]
-    level[last] = property.parse(level[last], property)
   }
 }
 

8652180e8ed734b471de6b12caaafdbea51b7fbe
Pau <frikeldon@gmail.com>
2022-02-08 00:45:01 +0100

    No se puede configurar el prefijo de los controles

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ export const components = {
 export default function install (app, { prefix = 'llesca' } = {}) {
   for (const name in components) {
     const kebabName = name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
-    app.component(`${prefix}-${kebabName}`, components[name])
+    app.component('llesca-' + kebabName, components[name])
   }
 }

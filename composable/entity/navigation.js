65a1500d79f44ed7a3954b912492bedad317dbe1
Pau <pau.drudis@gmail.com>
2022-10-08 12:14:33 +0200

    Por defecto entityParent se salta la colecciones

--- composable/entity/navigation.js
+++ composable/entity/navigation.js
@@ -1,11 +1,18 @@
+import { useEntityCollection } from './entity-collection.js'
 import { internalState } from './internal.js'
 
 export function entityRoot (entity) {
   return entity?.[internalState]?.root
 }
 
-export function entityParent (entity) {
-  return entity?.[internalState]?.parent
+export function entityParent (entity, { includeCollections = false } = {}) {
+  let parent = entity?.[internalState]?.parent
+  if (!includeCollections) {
+    while (parent instanceof useEntityCollection) {
+      parent = entity?.[internalState]?.parent
+    }
+  }
+  return parent
 }
 
 export function entityBase (entity) {

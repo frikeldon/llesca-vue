4c3585c9360b152ff8f827e21c2eee8a0bae47a9
Pau <pau.drudis@gmail.com>
2022-10-15 10:59:15 +0200

    Metodo para recuperar la definicion de una entidad

--- composable/entity/utils.js
+++ composable/entity/utils.js
@@ -1,4 +1,5 @@
 import { useEntity } from './entity.js'
+import { useEntityCollection } from './entity-collection.js'
 import { internalState } from './internal.js'
 
 export function entityPrimaryValue (entity) {
@@ -6,5 +7,14 @@ export function entityPrimaryValue (entity) {
     const state = entity[internalState]
     const primaryKey = state.definition.primaryKey
     return entity[primaryKey]
+  }
+}
+
+export function entityDefinition (entity) {
+  if (
+    entity instanceof useEntity ||
+    entity instanceof useEntityCollection
+  ) {
+    return entity[internalState].definition
   }
 }

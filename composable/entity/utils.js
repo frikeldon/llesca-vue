4f0a99b839e74b512686b346d0bf92f8f05e2ec4
Pau <pau.drudis@gmail.com>
2022-12-04 19:06:12 +0100

    entityHasPendingChanges

--- composable/entity/utils.js
+++ composable/entity/utils.js
@@ -18,3 +18,39 @@ export function entityDefinition (entity) {
     return entity[internalState].definition
   }
 }
+
+export function entityHasPendingChanges (
+  entity,
+  {
+    depth,
+    withoutChildren = false
+  } = {},
+  level = 0
+) {
+  const entityState = entity[internalState]
+
+  if (entity instanceof useEntity) {
+    if (Object.keys(entityState.changedProperties).length > 0) {
+      return true
+    }
+
+    if (!withoutChildren && (typeof depth !== 'number' || level < depth)) {
+      for (const key in entityState.children) {
+        if (entityHasPendingChanges(entityState.children[key], { depth, withoutChildren }, level + 1)) {
+          return true
+        }
+      }
+    }
+  } else if (entity instanceof useEntityCollection) {
+    if (entityState.newEntities.length > 0 || entityState.deletedIndices.size > 0) {
+      return true
+    }
+
+    for (const child of entityState.entities.value) {
+      if (entityHasPendingChanges(child, { depth, withoutChildren }, level)) {
+        return true
+      }
+    }
+  }
+  return false
+}
